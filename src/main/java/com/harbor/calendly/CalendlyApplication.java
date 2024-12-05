bf8d9d72fce1190b3b421f3da101e21cb6731c42
Abhishek Sharma <avsharma@tekion.com>
2024-12-05 16:39:39 +0530

    init

---
+++ src/main/java/com/harbor/calendly/CalendlyApplication.java
@@ -0,0 +1,14 @@
+package com.harbor.calendly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class for Calendly application
+ */
+@SpringBootApplication
+public class CalendlyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CalendlyApplication.class, args);
+    }
+}
